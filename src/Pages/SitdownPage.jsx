323dd8bafd413f74d151ec941b55a6d23d7d3070
wyswhsl21 <wyswhsl21@naver.com>
2022-11-08 00:03:47 +0900

    firstCommit

---
+++ src/Pages/SitdownPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SitdownPerson from "../Components/standSitdown/SitdownPerson";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+const SitdownPage = () => {
+  return (
+    <>
+      <Header />
+      <SitdownPerson />
+      <Footer />
+    </>
+  );
+};
+export default SitdownPage;
---
+++ src/Shared/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "../Pages/MainPage";
+import SitdownPage from "../Pages/SitdownPage";
+import StandPage from "../Pages/StandPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/sitdownpage" element={<SitdownPage />} />
+        <Route path="/standpage" element={<StandPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
