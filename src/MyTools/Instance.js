7a80c8bbbcdbe6a06019092fd7104430b9e43f66
HaeRheeNam <hanm3208@gmail.com>
2022-11-12 21:39:38 +0900

    남해리: 로그인 코드 다시 생성

---
+++ src/MyTools/Instance.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+
+const token = document.cookie.replace("token=", "");
+const accesstoken = token && jwt_decode(token);
+const id = accesstoken.userId;
+
+//instance 쓸 때는 headers값 안 넣어줘도 되지만,
+//axios로 따로 써줄 경우는 header 매번 넣어줘야 함.
+const instance = axios.create({
+  baseURL: "http://15.164.250.6:3000",
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+export const trainApi = {
+  postSignUps: (payload) => instance.post("/signup", payload),
+  postLogin: (payload) => instance.post("/auth/kakao/callback", payload),
+  // mypage: (payload) => instance.get(`/mypages/${id}`),
+  // personal: (payload) => instance.put(`/mypages/${id}/edit`, payload),
+
+  // getMenubar: (payload) => instance.get(`/mypages/main/${id}`),
+};
