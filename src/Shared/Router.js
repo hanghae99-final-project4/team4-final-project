d940f213c9162338128bac4cab4f86b817bbab97
Sanghyun Lee <shlee1027@daum.net>
2022-11-18 21:58:31 +0900

    이상현: package.josn 수정사항 추가

--- src/Shared/Router.js
+++ src/Shared/Router.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../Pages/MainPage";
-import SitdownPage from "../Pages/SitdownPage";
-import StandPage from "../Pages/StandPage";
 import MyPage from "../Components/Profile/Mypage";
 import ProfilePic from "../Components/Profile/ProfilePic";
 import Disclaimer from "../Components/Agreement/Disclaimer";
@@ -11,9 +9,7 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/sitdownpage" element={<SitdownPage />} />
-        <Route path="/standpage" element={<StandPage />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="profilepic" element={<ProfilePic />} />
         <Route path="/disclaimer" element={<Disclaimer />} />
