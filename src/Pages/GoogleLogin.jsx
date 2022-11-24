5bf41f07ace287b7b08b563b0d2554a4ff2b1fe3
HaeRheeNam <hanm3208@gmail.com>
2022-11-25 03:20:10 +0900

    남해리 : configstore 오류 잡고 이전 커밋 그대로 한 번 더 올림

--- src/Pages/GoogleLogin.jsx
+++ src/Pages/GoogleLogin.jsx
@@ -22,37 +22,46 @@ const GoogleLogin = () => {
   //   const accesstoken = token && jwtDecode(token);
   //   const id = accesstoken.userId;
 
-  useEffect(() => {
-    /* 인가코드 확인하기 */
-    const code = new URL(window.location.href).searchParams.get("code");
-    console.log("인가코드", code);
-    const yhURL = process.env.REACT_APP_YH_HOST;
-    //1. url에 뜬 인가코드 추출한 것 토큰 get요청 할 때 url 쿼리로 보내기.
-    // 2. 토큰(카카오토큰이든 자체 jwt토큰이든 )get으로 받기
+  /* 인가코드 확인하기 */
+  const code = new URL(window.location.href).searchParams.get("code");
+  console.log("인가코드", code);
+  const thURL = process.env.REACT_APP_TH__S_HOST;
+  //1. url에 뜬 인가코드 추출한 것 토큰 get요청 할 때 url 쿼리로 보내기.
+  // 2. 토큰(카카오토큰이든 자체 jwt토큰이든 )get으로 받기
 
-    axios
-      .get(
-        `${yhURL}/auth/google/callback?code=${code}`
-        //       {
-        //   headers: {
-        //     Authorization: `${token}`,
-        //   },
-        // }
-      )
-      .then((res) => {
-        console.log(res);
-        console.log(res.data);
-        console.log(res.data.jwtToken);
+  axios
+    .get(
+      `${thURL}/auth/google/callback?code=${code}`
+      //       {
+      //   headers: {
+      //     Authorization: `${token}`,
+      //   },
+      // }
+    )
+    .then((res) => {
+      // console.log(res);
+      // console.log(res.data);
+      // console.log(res.data.jwtToken);
 
-        const token = res.data.jwtToken;
-        if (token) {
-          setTokens("token", token, { path: "/" });
-          alert("로그인 하셨습니다");
-        }
-        window.location.replace("/main");
-      });
-  }, []);
-  return <div>빠른로딩중..</div>;
+      const token = res.data.jwtToken;
+      const msg = res.data.message;
+      const doneInfo = res.data.doneAdditionalInfo;
+      if (token) {
+        setTokens("token", token, { path: "/" });
+      }
+      if (doneInfo === true) {
+        navigator("/subwaypage");
+      } else {
+        navigator("/signup");
+      }
+      alert(`${msg}`);
+    });
+
+  return (
+    <div className="mx-[auto] my-[0px]">
+      <img src={FirstLogo} alt="firstlogo" />
+    </div>
+  );
 };
 
 export default GoogleLogin;
