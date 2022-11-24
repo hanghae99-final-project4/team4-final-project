4a554dd5ad0dd899724421ba1ba011db8c8f1628
HaeRheeNam <hanm3208@gmail.com>
2022-11-25 04:49:18 +0900

    남해리 : catch-error config오류 잡고 올림 1125 am 04:49

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./Redux/Config/Configstore";
+import store from "./Redux/Config/configstore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
