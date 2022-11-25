4b2c0a85f1f5af84d92b39c3c6165f5fa1a288b9
HaeRheeNam <hanm3208@gmail.com>
2022-11-26 04:15:40 +0900

    📌남해리: Feat- 로그인, 회원필수정보기입란, 공통메뉴바, 취소,다음버튼, 유효성검사 절반완료 1126 am 04:15

--- src/Pages/NaverLogin.jsx
+++ src/Pages/NaverLogin.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-import { trainApi } from "../MyTools/Instance";
 import { Cookies, useCookies } from "react-cookie";
-import jwtDecode from "jwt-decode";
 import FirstLogo from "../Assets/FirstLogo.svg";
 
 const NaverLogin = () => {
@@ -16,11 +14,11 @@ const NaverLogin = () => {
   // 인가코드 확인하기
   const code = new URL(window.location.href).searchParams.get("code");
   const state = new URL(window.location.href).searchParams.get("state");
-  console.log("인가코드", code);
+  // console.log("인가코드", code);
   // const yhURL = process.env.REACT_APP_YH_HOST;
   const thURL = process.env.REACT_APP_TH_S_HOST;
   // const thURL = process.env.REACT_APP_TH_HOST;
-  console.log(1);
+  // console.log(1);
   //1. url에 뜬 인가코드 추출한 것 토큰 get요청 할 때 url 쿼리로 보내기.
   // 2. 토큰(카카오토큰이든 자체 jwt토큰이든 )get으로 받기
   axios
@@ -33,10 +31,10 @@ const NaverLogin = () => {
       // }
     )
     .then((res) => {
-      console.log(res);
-      console.log(res.data);
-      console.log(res.data.jwtToken);
-      console.log(res.data.message);
+      // console.log(res);
+      // console.log(res.data);
+      // console.log(res.data.jwtToken);
+      // console.log(res.data.message);
 
       const token = res.data.jwtToken;
       const msg = res.data.message;
@@ -57,9 +55,8 @@ const NaverLogin = () => {
   //   });
   // // }, []);
   return (
-    <div className="mx-[auto] my-[0px]">
-      빠른로딩중..
-      <img src={FirstLogo} alt="firstlogo" />
+    <div className="flex justify-center items-center min-h-screen">
+      <img src={FirstLogo} alt="firstlogo" className="block m-[auto]" />
     </div>
   );
 };
