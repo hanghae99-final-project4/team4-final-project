e71d19702e7efdc442bfc76da5869b22caccb076
HaeRheeNam <hanm3208@gmail.com>
2022-12-02 10:13:05 +0900

    남해리 : tailwind config 반응형 설정 추가 1202am 10:13

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: { min: "375px" },
+      //=> @media (min-width: 375px){...}
+    },
     extend: {},
   },
   plugins: [],
