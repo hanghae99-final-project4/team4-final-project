ccdcd193c0351da18bbc0ddb6fabcb8efd16f9e0
wyswhsl21 <wyswhsl21@naver.com>
2022-12-09 18:37:05 +0900

    undefined 찾기

--- src/Components/Chatting/Chatting.jsx
+++ src/Components/Chatting/Chatting.jsx
@@ -208,7 +208,7 @@ const Chatting = () => {
       reset(initialState);
     }
   };
-  console.log(chatArr?.url, chatArr?.nickname);
+
   //이미지 비디오 보내는 로직
   async function postSend() {
     const formData = new FormData();
@@ -233,6 +233,7 @@ const Chatting = () => {
   const download = () => {
     window.location.assign();
   };
+  console.log(chatArr?.url, chatArr?.nickname, chatArr);
 
   return (
     <div
