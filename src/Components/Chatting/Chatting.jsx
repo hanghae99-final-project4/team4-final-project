abbf3d4c60e77ca084cd443013fe7ca3550b5ce5
wyswhsl21 <wyswhsl21@naver.com>
2022-12-09 18:28:11 +0900

    chatting 로직 수정

--- src/Components/Chatting/Chatting.jsx
+++ src/Components/Chatting/Chatting.jsx
@@ -191,20 +191,15 @@ const Chatting = () => {
       setFile([]);
     }
     console.log("여기까지 실행");
-    socket.emit(
-      "persnalchat",
-      {
-        url: chatArr?.url,
-        nickname: chatArr?.nickname,
-      },
-      console.log(chatArr?.url, chatArr?.nickname)
-    );
+
     if (message.msg !== "") {
       socket.emit("persnalchat", {
         roomkey: room,
         msg: message.msg,
         nickname: message.nickname,
         profile: message.url,
+        url: chatArr?.url,
+        nickname: chatArr?.nickname,
       });
       console.log("chatting", {
         roomkey: room,
@@ -214,6 +209,7 @@ const Chatting = () => {
       });
       reset(initialState);
     }
+    console.log(chatArr?.url, chatArr?.nickname);
   };
   //이미지 비디오 보내는 로직
   async function postSend() {
