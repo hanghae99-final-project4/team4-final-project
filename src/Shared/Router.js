43adfb7032f931c57ed7ac28581b20ca48ac1844
kimjaewoo <wyswhsl21@naver.com>
2023-04-02 18:30:27 +0900

    refactor: 리팩토링

--- src/Shared/Router.js
+++ src/Shared/Router.js
@@ -6,7 +6,6 @@ import ChatPage from "../Pages/ChatPage";
 import MainPage from "../Pages/MainPage";
 import ProfilePic from "../Components/Profile/ProfilePic";
 import Disclaimer from "../Components/Agreement/Disclaimer";
-import ConversPage from "../Pages/ConversPage";
 import ChattingPage from "../Pages/ChattingPage";
 import SubwayPage from "../Pages/SubwayPage";
 import { Suspense, lazy } from "react";
@@ -18,34 +17,34 @@ import AddInfo from "../Pages/AddInfo";
 import SubSign from "../Pages/SubSign";
 
 const LoginPage = lazy(() => import("../Pages/Login"));
-const ConversPage =lazy(() => import("../Pages/ConversPage"));
-const MyPage =lazy(() => import("../Components/Profile/Mypage"));
+const ConversPage = lazy(() => import("../Pages/ConversPage"));
+const MyPage = lazy(() => import("../Components/Profile/Mypage"));
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/subsign" element={<SubSign />} />
-        <Route path="/authcode" element={<AuthCode />} />
-        <Route path="/addinfo" element={<AddInfo />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="profilepic" element={<ProfilePic />} />
-        <Route path="/disclaimer" element={<Disclaimer />} />
-        <Route path="/auth/naver/callback" element={<NaverLogin />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/chatpage" element={<ChatPage />} />
-        <Route path="/converspage" element={<ConversPage />} />
-        <Route path="/chattingpage" element={<ChattingPage />} />
-        <Route path="/subwaypage" element={<SubwayPage />} />
-        <Route path="/customeruserguide" element={<CustomerUserGuide />} />
-        <Route path="/customernotice" element={<CustomerNotice />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/subsign" element={<SubSign />} />
+          <Route path="/authcode" element={<AuthCode />} />
+          <Route path="/addinfo" element={<AddInfo />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="profilepic" element={<ProfilePic />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
+          <Route path="/auth/naver/callback" element={<NaverLogin />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/chatpage" element={<ChatPage />} />
+          <Route path="/converspage" element={<ConversPage />} />
+          <Route path="/chattingpage" element={<ChattingPage />} />
+          <Route path="/subwaypage" element={<SubwayPage />} />
+          <Route path="/customeruserguide" element={<CustomerUserGuide />} />
+          <Route path="/customernotice" element={<CustomerNotice />} />
 
-        <Route path="/agreepage" element={<AgreePage />} />
-      </Routes>
-     </Suspense>
+          <Route path="/agreepage" element={<AgreePage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
