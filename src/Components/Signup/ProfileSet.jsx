8fc5c795343b458e63242e40f183819da5659bf1
kimjaewoo <wyswhsl21@naver.com>
2023-05-01 21:49:22 +0900

    refactor: 회원가입 시 프로필 설정 단계까지 작업 완료

--- src/Components/Signup/ProfileSet.jsx
+++ src/Components/Signup/ProfileSet.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import progress from "../../Assets/SetProfile/nextprogress.svg";
+import pendingbutton from "../../Assets/SetProfile/pendingbutton.svg";
 import startbutton from "../../Assets/SetProfile/startbutton.svg";
 import { useNavigate } from "react-router-dom";
 import avatar from "../../Assets/SetProfile/avatar.svg";
+import upload from "../../Assets/SetProfile/profile.svg";
 import { useRef } from "react";
+import { useRecoilState } from "recoil";
+import { usePrimaryState } from "../../Recoil/userList";
+import useInput from "../../MyTools/Hooks/UseInput";
 const ProfileSet = () => {
+  const [profile, setProfile] = useRecoilState(usePrimaryState);
+  const [form, setForm, OnChangeHandler] = useInput([]);
   const fileref = useRef();
   const navigate = useNavigate();
 
   const profileuploadHandler = () => {
     navigate("/pickprofile");
   };
-
+  console.log(form);
   return (
     <Wrap>
       <GifBox>
@@ -24,11 +31,43 @@ const ProfileSet = () => {
             프로필을 설정해주세요.
           </Profile>
         </SpanBox>
-        <Avatar onClick={profileuploadHandler} src={avatar} alt="avatar" />
+        {/* primary image 가 있으면 ? */}
+        {profile[0]?.url && true ? (
+          <>
+            <Avatar
+              onClick={profileuploadHandler}
+              src={profile[0]?.url}
+              alt="avatar"
+            />
+            <Upload src={upload} alt="upload" />
+          </>
+        ) : (
+          <>
+            <Avatar onClick={profileuploadHandler} src={avatar} alt="avatar" />
+            <Upload src={upload} alt="upload" />
+          </>
+        )}
       </GifBox>
-      <Nickname placeholder="사용하실 닉네임" />
+      <Nickname
+        onChange={OnChangeHandler}
+        value={form.nickname}
+        name="nickname"
+        placeholder="사용하실 닉네임"
+      />
+      {profile[0]?.url && true && form.nickname && true ? (
+        <>
+          <StartButton
+            style={{ cursor: "pointer" }}
+            src={startbutton}
+            alt="startimg"
+          />
+        </>
+      ) : (
+        <>
+          <StartButton src={pendingbutton} alt="startimg" />
+        </>
+      )}
 
-      <StartButton src={startbutton} alt="startimg" />
       <StartSpan>시작</StartSpan>
     </Wrap>
   );
@@ -64,6 +103,7 @@ const SpanBox = styled.div`
   align-items: center;
 `;
 const Avatar = styled.img`
+  border-radius: 50%;
   cursor: pointer;
   margin-top: 29px;
   width: 100px;
@@ -99,4 +139,8 @@ const StartButton = styled.img`
 `;
 const StartSpan = styled.span`
   margin-top: 10px;
+`;
+const Upload = styled.img`
+  margin-left: 79px;
+  margin-top: -34px;
 `;
